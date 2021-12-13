43c90b020fa9780422f0ec84c5798bf4f6b6dd53
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-12-13 21:38:33 +0700

    Mock Get Contract Detail

--- cli/Erc20Helper.py
+++ cli/Erc20Helper.py
@@ -1,8 +1,31 @@
 import argparse
 
+def get_detail(args):
+    addr = args.detail[0]
+    mock_detail = {
+        "name": "BOOM",
+        "symbol": "BM",
+        "decimals": 18
+    }
+    print(mock_detail)
+    return
+
 def main():
     arg_parser = argparse.ArgumentParser(description="TEST ARG_PARSER")
-    arg_parser.parse_args()
+    arg_parser.add_argument(
+        "--detail", 
+        type = str, 
+        nargs = 1,
+        metavar = "<contract_address>",
+        default = None,
+        help = "Get Token's name, symbol and decimals from Contract Address"
+        )
+    args = arg_parser.parse_args()
+
+    if args.detail != None: 
+        get_detail(args)
+    else:
+        "NOT IMPLEMENTED"
 
 if __name__ == "__main__":
     main()
