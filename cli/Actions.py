55a20e768c9a458223fa5c4b93228d961d280d19
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-12-15 01:56:21 +0700

    mock function

--- cli/Actions.py
+++ cli/Actions.py
@@ -1,11 +1,26 @@
 import web3
 
-def get_detail(args):
-    addr = args.detail[0]
+def get_detail(contract_addr):
     mock_detail = {
         "name": "BOOM",
         "symbol": "BM",
         "decimals": 18
     }
     print(mock_detail)
+    return
+
+def get_balanceOf(contract_addr, target_addr):
+    print(f"balanceOf {contract_addr}, {target_addr}")
+    return
+
+def subscribe_tx(contract_addr):
+    print(f"subscribe {contract_addr}")
+    return
+
+def get_latest_tx(n, contract_addr):
+    print(f"get {n} latest tx of {contract_addr}")
+    return
+
+def get_top_holders(n, contract_addr):
+    print(f"get {n} top holders of {contract_addr}")
     return
