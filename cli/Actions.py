215ff16b6e85c08ca2a81b685d1feb1c76b67ef0
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-12-15 18:02:54 +0700

    Complete BalanceOf

--- cli/Actions.py
+++ cli/Actions.py
@@ -1,15 +1,15 @@
 from ERC20Token import ERC20Contract
 
-
 def get_detail(contract_addr):
     contract = ERC20Contract(contract_addr)
-    print(contract.name)
-    print(contract.symbol)
-    print(contract.decimals)
+    print(f'Name: {contract.name}')
+    print(f'Symbol: {contract.symbol}')
+    print(f'Decimals: {contract.decimals}')
     return
 
 def get_balanceOf(contract_addr, target_addr):
-    print(f"balanceOf {contract_addr}, {target_addr}")
+    contract = ERC20Contract(contract_addr)
+    print(contract.balanceOf(target_addr))
     return
 
 def subscribe_tx(contract_addr):
--- cli/ERC20Token.py
+++ cli/ERC20Token.py
@@ -18,9 +18,9 @@ class ERC20Contract:
         if not Web3.isAddress(addr):
             sys.stderr.write('Invalid Contract Address\n')
             sys.exit(2)
-
+        addr = Web3.toChecksumAddress(addr)
+        self.contract = w3.eth.contract(address=addr, abi=ERC20_ABI)
         try:
-            self.contract = w3.eth.contract(address=addr, abi=ERC20_ABI)
             self.name = self.contract.functions.name().call()
             self.symbol = self.contract.functions.symbol().call()
             self.decimals = self.contract.functions.decimals().call()
@@ -28,4 +28,26 @@ class ERC20Contract:
         except:
             sys.stderr.write('Invalid Contract Type\n')
             sys.exit(2)
+    
+    def uint2decimal(self, value):
+        if value == 0: 
+            decimal = 0
+        
+        ### EX. value = 100, self.decimals = 4 => decimal = "0.0100"  
+        elif len(str(value)) <= self.decimals:
+            decimal = f'0.{"0"*(self.decimals - len(str(value)))}{value}'
+
+        ### EX. value = 1000, self.decimals = 2 => decimal = "10.00" 
+        else:
+            decimal = str(value)[:len(str(value)) - self.decimals] + '.' + str(value)[len(str(value)) - self.decimals:]
+            
+        return float(decimal)
+
+    def balanceOf(self, addr):
+        if not Web3.isAddress(addr):
+            sys.stderr.write('Invalid Tartget Address\n')
+            sys.exit(2)
+        balance = self.contract.functions.balanceOf(addr).call()
+        return f'Balance of {self.name}: {self.uint2decimal(balance)}'
+        
 
